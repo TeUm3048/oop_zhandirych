4fb92d4729990e78c080666114c5f628be54b127
Iwan Ryžij <teum3048@yandex.ru>
2023-09-25 21:48:33 +0300

    Add comments

--- src/Observable/Observable.h
+++ src/Observable/Observable.h
@@ -7,8 +7,13 @@
 
 class Observable {
 public:
+// Add an observer to the list of Observers for this Observable object.
     void addObserver(Observer *observer);
 
+
+// The `notifyUpdate()` function is used to notify all the Observers that an update has occurred.
+// This function will run `update()` method on each Observer,
+// which added for this Observable object.
     void notifyUpdate();
 
 private:
--- src/Observer/Observer.h
+++ src/Observer/Observer.h
@@ -4,6 +4,15 @@
 
 class Observer {
 public:
+//  The `virtual` keyword is used to declare a virtual function in a base class.
+//  A virtual function is a member function that can be overridden
+//  in a derived class.
+//  When a virtual function is called through a pointer or reference
+//  to the base class, the actual function called is determined at runtime
+//  based on the type of the object being pointed to or referenced.
+//  This allows for dynamic polymorphism, where different derived classes can
+//  provide different implementations of the same function.
+
     virtual void update() = 0;
 };
 
