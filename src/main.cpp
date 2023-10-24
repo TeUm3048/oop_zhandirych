4458f8e63f4af5be04556f8dc16001b9aa78559a
Iwan Ryžij <teum3048@yandex.ru>
2023-10-24 21:32:00 +0300

    Fix windows utf8

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,27 @@
 #include "PlayerView/PlayerView.h"
 #include "PlayerController/PlayerController.h"
 #include "FieldCreator/FieldCreator.h"
+void updateLocale() ;
 
+#ifdef _WIN32
+
+#include <windows.h> // для функции SetConsoleOutputCP и константы CP_UTF8
+
+void updateLocale() {
+    std::setlocale(LC_ALL, "ru_RU.UTF8");
+
+    SetConsoleOutputCP(CP_UTF8);
+}
+
+#elif
+void updateLocale() {
+    std::setlocale(LC_ALL, "ru_RU.UTF8");
+}
+#endif
 
 int main(int argc, char const *argv[]) {
-    std::setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    updateLocale();
     Player player;
     FieldCreator fieldCreator;
     Field field = fieldCreator.createFieldForLevel(2);
